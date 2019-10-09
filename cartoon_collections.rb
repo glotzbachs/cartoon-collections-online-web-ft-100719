98c0e1786cc2de25a47390e802b08d181aa6254b
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-09 19:25:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+    array.each_with_index do |name,index|
+      puts "#{index+1}. #{name}"
+    end
+    array
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map{|string| string.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any?{|call| call.length>4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
+  
+  
 end
+
+#original thoughts for find_the_cheese
+  # if array.include?("gouda")
+  #   return "gouda"
+  # elsif array.include?("cheddar")
+  #   return "cheddar"
+  # elsif array.include?("camembert")
+  #   return "camembert"
+  # else
+  #   return nil
+  # end
